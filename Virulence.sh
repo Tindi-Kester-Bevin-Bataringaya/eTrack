c7e5af0497990f46818cbe63ffd620c23eef8bf1
PETER96-11 <sabakakipeterz@gmail.com>
2020-08-11 10:00:30 +0300

    Remove unwanted files

--- Virulence.sh
+++
@@ -1,5 +0,0 @@
-#! bin/bash
-for i in `cat list.txt`
-do      abricate --csv --db vfdb /home/akiyaga/Miniproject/assembly/${i}_out/assembly.fasta > Virulence/${i}_virulence.csv
-done
-abricate --summary Virulence/*.csv > Virulence.csv
