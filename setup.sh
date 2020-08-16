c29f2c04af8193faa20ba2de6d1d45a44f298145
GunzIvan28 <ivangunz23@gmail.com>
2020-08-16 23:21:11 +0300

    Updated final script README and full setup and dependancies

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+export PATH=~/miniconda3/bin:$PATH
+cp -rf config-files ../miniconda3/envs/eTrack-1.0/
+chmod 777 ../miniconda3/envs/eTrack-1.0/files/*
+cp -rf bin/eTrack ../miniconda3/envs/eTrack-1.0/bin/
+chmod 777 ../miniconda3/envs/eTrack-1.0/bin/eTrack
+
+###clean-up
+echo -e "eTrack is all set up!!! Run 'eTrack -h' to confirm."
+touch ../clean.sh &
+echo "#! /usr/bin/env bash" >../clean.sh
+echo "rm -rf eTrack" >>../clean.sh
+chmod 777 ../clean.sh
+cd
+bash clean.sh
